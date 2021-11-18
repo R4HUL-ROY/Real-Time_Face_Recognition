22a8ec6ed2b8bbb596c61fff7244532e4b659c19
Rahul Roy <rahul410roy@gmail.com>
2021-11-18 19:40:00 +0530

    finishing collect_facedata.py

--- collect_facedata.py
+++ collect_facedata.py
@@ -15,15 +15,41 @@ cam = cv2.VideoCapture(0)
 cam.set(3,640)
 cam.set(4,480)
 
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+# Face detector using haarcascade classifier
+# more such classifiers are present in - https://github.com/opencv/opencv/tree/master/data/haarcascades
 face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+# for each new person, enter new numeric user id
+face_id = input("Enter user id : ")
+print("Initializing face capture, Look at the camera and please wait")
+
+count = 0
 
 while True:
     ret, img = cam.read()
     img = cv2.flip(img, 1) # Flip vertically
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    cv2.imshow('camera', img)
+    faces = face_detector.detectMultiScale(gray, 1.3, 5)
+
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0), 2)
+        count += 1
+
+        cv2.imwrite("dataset/user." + str(face_id) + "." + str(count) + ".jpg", gray[y:y+h, x:x+w])
+        cv2.putText(img, str(count) + " Taken", (x+5,y-5), font, 1, (255,255,255), 2)
+        cv2.imshow('image', img)
+        cv2.setWindowProperty('image', cv2.WND_PROP_TOPMOST, 1)
+        
+
     k = cv2.waitKey(10) & 0xff # Press 'ESC' for exiting video
     if k == 27:
         break
+    elif count >= 50:
+        break
+
+print("Exiting Program")
 cam.release()
 cv2.destroyAllWindows()
